9446ec59f47db912b1cc11e178ab06821a8b9324
Andrey Karpov <ndk@users.noreply.github.com>
2021-01-15 18:33:37 +0300

    Add some clarification to the code

--- internal/materializer/service.go
+++ internal/materializer/service.go
@@ -28,19 +28,22 @@ func (s *service) Execute() error {
 		Consumer: s.cfg.Consumer,
 		Streams:  []string{s.cfg.Stream, ">"},
 	}
+	// The worker's purpose is fetching messages from the stream and put them into a corresponding place in Redis.
 	for {
 		streams, err := s.client.XReadGroup(ctx, readArg).Result()
 		if err != nil {
 			return fmt.Errorf("couldn't read a group: %w", err)
 		}
+		// It mustn't happen because we're waiting for data only from a single source.
 		if len(streams) != 1 {
 			return fmt.Errorf("unexpected number of streams: %d", len(streams))
 		}
 
+		// We've got a bunch of messages, so the next step is extracting original posts.
 		for _, message := range streams[0].Messages {
 			blob, ok := message.Values[storage.StreamValueField].(string)
 			if !ok {
-				//TODO What will we do with the other messages?
+				//TODO What will we do with the other messages? I believe it's a place for variate decisions.
 				return fmt.Errorf("couldn't find an event in a message: %v", message)
 			}
 			var post protocol.Post
@@ -49,11 +52,13 @@ func (s *service) Execute() error {
 			}
 
 			if post.Promoted {
+				// Promoted posts go to a circular list.
 				if err := s.client.LPush(ctx, s.cfg.Promotion, blob).Err(); err != nil {
 					return fmt.Errorf("couldn't put a promoted post into a ring: %w", err)
 				}
 				continue
 			}
+			// Ordinary posts should be kept in a sorted set.
 			if err := s.client.ZAdd(ctx, s.cfg.Feed, &redis.Z{
 				Score:  float64(post.Score),
 				Member: blob,
